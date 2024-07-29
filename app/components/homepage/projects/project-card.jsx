635b7b63f5a981900683cef6217d51edf49e08e6
Diptajit Saha <sahadiptajit.2020@gmail.com>
2024-07-30 00:59:49 +0530

    update project-card color

--- app/components/homepage/projects/project-card.jsx
+++ app/components/homepage/projects/project-card.jsx
@@ -12,9 +12,9 @@ function ProjectCard({ project }) {
       </div>
       <div className="px-4 lg:px-8 py-3 lg:py-5 relative">
         <div className="flex flex-row space-x-1 lg:space-x-2 absolute top-1/2 -translate-y-1/2">
-          <div className="h-2 w-2 lg:h-3 lg:w-3 rounded-full bg-red-400"></div>
-          <div className="h-2 w-2 lg:h-3 lg:w-3 rounded-full bg-orange-400"></div>
-          <div className="h-2 w-2 lg:h-3 lg:w-3 rounded-full bg-green-200"></div>
+          <div className="h-2 w-2 lg:h-3 lg:w-3 rounded-full bg-red-500"></div>
+          <div className="h-2 w-2 lg:h-3 lg:w-3 rounded-full bg-orange-500"></div>
+          <div className="h-2 w-2 lg:h-3 lg:w-3 rounded-full bg-green-500"></div>
         </div>
         <p className="text-center ml-3 text-[#16f2b3] text-base lg:text-xl">
           {project.name}
