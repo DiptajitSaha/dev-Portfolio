7f2599d66ec99803b1867be49295a3675a72f646
Diptajit Saha <sahadiptajit.2020@gmail.com>
2024-07-30 00:55:49 +0530

    init

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -2,7 +2,7 @@ export const personalData = {
   name: "DIPTAJIT SAHA",
   profile: '/profile.jpeg',
   designation: "Software Developer",
-  description: "I'm Diptajit Saha,a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
+  description: "I'm Diptajit Saha, an aspiring full stack developer eager to solve problems that are presented. I have successfully built three full stack projects and am working on more. I love to learn and explore new technologies and working and contributing in a team. I am available for any kind of job opportunity that suits my skills and interests.",
   email: 'sahadiptajit.2020@gmail.com',
   phone: '+91 84200 07111',
   address: 'Beleghata, Kolkata, India 70010',
