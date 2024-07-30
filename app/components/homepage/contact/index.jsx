b96d8734d800458a1ddccab398ccecbe12958ae0
Diptajit Saha <sahadiptajit.2020@gmail.com>
2024-07-31 01:49:28 +0530

    edit test colors

--- app/components/homepage/contact/index.jsx
+++ app/components/homepage/contact/index.jsx
@@ -6,8 +6,6 @@ import { CiLocationOn } from "react-icons/ci";
 import { FaXTwitter } from "react-icons/fa6";
 import { IoLogoGithub, IoMdCall } from "react-icons/io";
 import { MdAlternateEmail } from "react-icons/md";
-import ContactWithCaptcha from './contact-with-captcha';
-import ContactWithoutCaptcha from './contact-without-captcha';
 
 function ContactSection() {
   return (
@@ -26,6 +24,7 @@ function ContactSection() {
                 <MdAlternateEmail
                   className="bg-[#8b98a5] p-2 rounded-full hover:bg-[#16f2b3] hover:scale-110 transition-all duration-300 text-gray-800 cursor-pointer"
                   size={36}
+                  
                 />
                 <span>{personalData.email}</span>
               </p>
--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -9,7 +9,7 @@ function Navbar() {
         <div className="flex flex-shrink-0 items-center">
           <Link
             href="https://github.com/DiptajitSaha" target = "_blank"
-            className=" text-[#16f2b3] text-3xl font-bold">
+            className=" bg-gradient-to-r from-yellow-600 to-purple-400 inline-block text-transparent bg-clip-text text-4xl font-bold">
             DIPTAJIT SAHA
           </Link>
         </div>
@@ -21,13 +21,13 @@ function Navbar() {
             </Link>
           </li>
           <li>
-            <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/#skills"><div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">SKILLS</div></Link>
+            <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/#skills"><div className="text-sm text-white transition-colors duration-300 hover:text-green-600">SKILLS</div></Link>
           </li>
           <li>
-            <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/#education"><div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">EDUCATION</div></Link>
+            <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/#education"><div className="text-sm text-white transition-colors duration-300 hover:text-red-600">EDUCATION</div></Link>
           </li>
           <li>
-            <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/#projects"><div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">PROJECTS</div></Link>
+            <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/#projects"><div className="text-sm text-white transition-colors duration-300 hover:text-yellow-600">PROJECTS</div></Link>
           </li>
         </ul>
       </div>
